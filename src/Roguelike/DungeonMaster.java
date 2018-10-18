313d4cf49f87e699c574d74cafa863afa86806e6
aqeynnkn <aqeynnkn@sharklasers.com>
2018-10-18 17:33:49 +0200

    Test

--- src/Roguelike/DungeonMaster.java
+++ src/Roguelike/DungeonMaster.java
@@ -2,6 +2,9 @@ package Roguelike;
 
 //DungeonMaster är för stunden bara en dummyklass för att möjliggöra testande i Character. Lägg till vadhelst som behövs
 //i den så länge som det inte krockar med något annat.
+
+//Testkommentar
+
 public class DungeonMaster{
     
     //Direction indikerar riktningen i vilken handlingen utförs, där 1 är norrut och sedan medsols till 4, som är
