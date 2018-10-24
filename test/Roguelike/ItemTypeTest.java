2ffed58e1c8c9b8f5838b45a9639a8f5b9c3b010
aqeynnkn <aqeynnkn@sharklasers.com>
2018-10-24 12:04:57 +0200

    Implementerade ItemType

--- test/Roguelike/ItemTypeTest.java
+++ test/Roguelike/ItemTypeTest.java
@@ -1,4 +1,40 @@
 package Roguelike;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class ItemTypeTest {
+    ItemType t;
+    
+    private void setup(){
+        t = new ItemType(false, false, false, false, false);
+    }
+    @Test
+    void isWeapon(){
+        setup();
+        assertEquals(false, t.isWeapon());
+    }
+    @Test
+    void isArmor(){
+        setup();
+        assertEquals(false, t.isArmor());
+    }
+    @Test
+    void isHelmet(){
+        setup();
+        assertEquals(false, t.isHelmet());
+    }
+    @Test
+    void isConsumable(){
+        setup();
+        assertEquals(false, t.isConsumable());
+    }
+    @Test
+    void isTrinket(){
+        setup();
+        assertEquals(false, t.isTrinket());
+    }
+    
+    
 }
